630de3571025219694c9af225f2a780e5b313abc
AregShahbazian <areg.uvagm@gmail.com>
2020-04-20 18:25:49 +0800

    deep linking

--- App.js
+++ App.js
@@ -1,85 +1,91 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
+import * as React from 'react';
+import {Button, Linking, Text, View} from 'react-native';
+import {NavigationContainer, useLinking} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
 
-import React from 'react';
-import {StyleSheet} from 'react-native';
+const navigationRef = React.createRef();
 
-import {Colors} from 'react-native/Libraries/NewAppScreen';
-
-import {createStore} from 'redux';
-import Cont from './Container';
-import {NavigationContainer} from '@react-navigation/native';
-import {isMountedRef, navigationRef} from './RootNavigation';
-
-function counter(state = 10, action) {
-  switch (action.type) {
-    case 'INCREMENT':
-      return state + 1;
-    case 'DECREMENT':
-      return state - 1;
-    default:
-      return state;
-  }
+function navigate(name, params) {
+  navigationRef.current && navigationRef.current.navigate(name, params);
 }
 
-let store = createStore(counter);
+const RootStack = createStackNavigator();
 
-const App: () => React$Node = () => {
+export default function App() {
+  const {getInitialState} = useLinking(navigationRef, {
+    prefixes: ['https://testapp.com', 'testapp://'],
+  });
+
+  const [isReady, setIsReady] = React.useState(false);
+  const [initialState, setInitialState] = React.useState();
+
   React.useEffect(() => {
-    isMountedRef.current = true;
+    Promise.race([
+      getInitialState(),
+      new Promise(resolve =>
+        // Timeout in 150ms if `getInitialState` doesn't resolve
+        // Workaround for https://github.com/facebook/react-native/issues/25675
+        setTimeout(resolve, 150),
+      ),
+    ])
+      .catch(e => {
+        console.error(e);
+      })
+      .then(state => {
+        console.log('then');
+        if (state !== undefined) {
+          setInitialState(state);
+        }
 
-    return () => (isMountedRef.current = false);
-  }, []);
+        setIsReady(true);
+      });
+  }, [getInitialState]);
+
+  if (!isReady) {
+    return null;
+  }
+
+  console.log({initialState});
 
   return (
-    <NavigationContainer ref={navigationRef}>
-      <Cont/>
+    <NavigationContainer ref={navigationRef} initialState={initialState}>
+      <RootStack.Navigator>
+        <RootStack.Screen name="Home">
+          {props => {
+            return (
+              <View
+                style={{
+                  flex: 1,
+                  alignItems: 'center',
+                  justifyContent: 'center',
+                }}>
+                <Button
+                  title="LINK"
+                  onPress={() => {
+                    console.log('PRESS');
+                    Linking.openURL('testapp://Settings');
+                  }}
+                />
+              </View>
+            );
+          }}
+        </RootStack.Screen>
+        <RootStack.Screen name="Settings">
+          {props => {
+            return (
+              <View
+                style={{
+                  flex: 1,
+                  alignItems: 'center',
+                  justifyContent: 'center',
+                }}>
+                <Text>Hello {props.route.params.userName}</Text>
+                <Button title="Go to Home" onPress={() => navigate('Home')} />
+              </View>
+            );
+          }}
+        </RootStack.Screen>
+      </RootStack.Navigator>
     </NavigationContainer>
   );
-};
-
-const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
-});
-
-export default App;
+}
